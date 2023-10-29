dc9c0ef4c657cd208a0614ff6acb1a1782c85db6
richhdev <79666708+richhdev@users.noreply.github.com>
2023-10-29 17:32:59 +1100

    📈 add vercel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   NextScript,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { Analytics } from "@vercel/analytics/react";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -36,6 +37,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );
